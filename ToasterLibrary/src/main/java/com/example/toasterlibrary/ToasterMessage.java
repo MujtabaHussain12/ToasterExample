fb298d4c1a0583062bd9badb3cfd6c1c87134dbd
MujtabaHussain12 <mujtabahussian98@gmail.com>
2021-12-10 12:28:10 +0500

    hye

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -5,9 +5,12 @@ import android.widget.Toast;
 
 public class ToasterMessage {
 
-    public static void s(Context c, String message) {
+
+    public static void show(Context c, String message) {
 
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
     }
-}12
+}
+
+
 
